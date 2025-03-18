f4838fd807ac32b205d8f6a95a527ad614c9ece2
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-18 15:57:08 +0700

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,34 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AuthProvider from "./context/auth-context";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/home";
+import DashboardPage from "./routes/dashboard";
+import RegisterPage from "./routes/register";
+import LoginPage from "./routes/login";
+import PrivateRoute from "./components/private-route";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/dashboard",
+    element: <PrivateRoute />,
+    children: [
+      {
+        path: "",
+        element: <DashboardPage />,
+      },
+    ],
   },
 ]);
 
