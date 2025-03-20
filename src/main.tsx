43c07d1a8677bdbef3a32c1dc4b0abdf32a2e568
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-03-20 07:14:01 +0700

    feat: add tanstack-query config

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import DashboardPage from "./routes/dashboard";
 import RegisterPage from "./routes/register";
 import LoginPage from "./routes/login";
 import PrivateRoute from "./components/private-route";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -37,7 +40,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
 );
